4df988c3eb6d8e7690d1e4a1fa670eb7f55c8333
ViniciusMizani <30930359+ViniciusMizani@users.noreply.github.com>
2019-04-29 13:09:06 -0300

    .

--- app/src/main/java/com/example/projeto1/MainActivity.java
+++ app/src/main/java/com/example/projeto1/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.projeto1;
 
 import android.content.Intent;
+import android.support.v4.view.ViewPropertyAnimatorListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -14,11 +15,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Button btnIncluir = (Button)findViewById(R.id.btnIncluir);
         btnIncluir.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick (View v) {
-                Intent intent = new Intent(MainActivity.this, incluirAluno.class);
-                startActivity(intent);
-            }
+           @Override
+           public void onClick (View v) {
+               Intent intent = new Intent(MainActivity.this, incluirAluno.class);
+               startActivity(intent);
+           }
         });
     }
+
 }
